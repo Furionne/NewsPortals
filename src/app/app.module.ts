334e703517a1521acf7428bb2f01591efcb394fe
Cláudio Amaral <claudiorafaelamaral@gmail.com>
2022-12-06 11:14:56 +0000

    new components pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { HighlightComponent } from './pages/highlight/highlight.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArticleComponent,
+    HighlightComponent
   ],
   imports: [
     BrowserModule,
